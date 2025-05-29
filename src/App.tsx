978d631aefe540556e1ec124f728f1f3485ae238
skyar61 <ooosklyar@gmail.com>
2025-05-29 16:41:06 +0300

    chore: add package.json deps

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeroSection from './components/HeroSection';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <HeroSection />
+      {/* Далее: секция Направления и Реализованные проекты */}
+    </div>
+  );
+}
+
+export default App;
